198f185c6ee225c5cf23d652fa7312e7ffc9a330
experimentallife <user@experimentallife.dev>
2022-05-16 21:20:04 -0500

    :sparkles: cycle short

--- cycle-short/main.cpp
+++ cycle-short/main.cpp
@@ -1,91 +1,79 @@
-// Cycle sort
 #include <iostream>
-using namespace std;
-
-
-void cycleSort(int arrayOfNumbers[], int numberOfElements)
-{
-    int writes = 0;
-
-    for (int cycle_start = 0; cycle_start <= numberOfElements - 2; cycle_start++)
-    {
-        // initialize the value of the first element
-        int element = arrayOfNumbers[cycle_start];
-
-        // Count all smaller values than our element
-        int position = cycle_start;
-        for (int i = cycle_start + 1; i < numberOfElements; i++)
-        {
-            if (arrayOfNumbers[i] < element)
-            {
-                position++;
-            }
-        }
-
-        // If the element is already in the correct position
-        if (position == cycle_start)
-        {
-            continue;
-        }
-
-        // Ignore duplicate elements
-        while (element == arrayOfNumbers[position])
-        {
-             position += 1;
-        }
-
-        // Put the value in the right position
-        if (position != cycle_start)
-        {
-            swap(element, arrayOfNumbers[position]);
-            writes++;
-        }
-
-        // Cycle for the rest of elements
-        while (position != cycle_start)
-        {
-            position = cycle_start;
-
-            // Find position where we put the element
-            for (int i = cycle_start + 1; i < numberOfElements; i++)
-            {
-                if (arrayOfNumbers[i] < element)
-                {
-                    position += 1;
-                }
-            }
-
-            // Ignore duplicate elements
-            while (element == arrayOfNumbers[position])
-            {
-                position += 1;
-            }
-
-            // Put the value in the right position
-            if (element != arrayOfNumbers[position])
-            {
-                swap(element, arrayOfNumbers[position]);
-                writes++;
-            }
-        }
-    }
-}
-
-void printElementsOfArray(int arrayOfNumbers[], int numberOfElements)
-{
-  for (int i = 0; i < numberOfElements; ++i)
-  {
-    cout << arrayOfNumbers[i] << " ";
-  }
-  cout << endl;
-}
+#include <time.h>
 
 int main()
 {
-  int arrayOfNumbers[] = {4, 7, 9, 3, 21, 18, 23, 15, 8, 17};
-  int numberOfElements = sizeof(arrayOfNumbers) / sizeof(arrayOfNumbers[0]);
+  srand(time(0));
 
-  cycleSort(arrayOfNumbers, numberOfElements);
-  cout<<"The sorted array is: \n";
-  printElementsOfArray(arrayOfNumbers, numberOfElements);
+  int size = 10;
+  int data[size];// = { 90, 45, 30, 20, 10, 40, 60, 50, 5, 2 }; //caso de prueba manual
+  
+  // generamos datos aleatorios
+  std::cout << "Antes de ordenar: " << std::endl;
+  for(int* e = data; e < data + size; e++) {
+    *e = rand() & 100; // comentar linea para caso de prueba manual
+    std::cout << *e << " ";
+  }
+
+  std::cout << "\n\nciclo: {ciclo} -> item = {antes del cambio} {despues del cambio}" << std::endl;
+
+  for(int* cycleStart = data; cycleStart < data + (size - 1); cycleStart++) {
+    // inicializamos el valor del primer elemento
+    int element = *cycleStart;
+    int* pointer = cycleStart;
+
+    // Cuenta todos los valores más pequeños que nuestro elemento
+    for(int* nextElement = cycleStart + 1; nextElement < data + size; nextElement++) {
+      if(*nextElement < element)
+        pointer++;
+    }
+
+    // Si el elemento ya está en la posición correcta
+    if(pointer - data == cycleStart - data) {
+      std::cout << "\nciclo: " << (cycleStart - data) << " -> item = "<< *pointer << " ya está en su posición";
+      continue;
+    }
+
+    // Ignorar elementos duplicados
+    while(element == *pointer)
+      pointer++;
+
+    // Poner el valor en la posición correcta
+    if (pointer - data != cycleStart - data) {
+      std::cout << "\nciclo: " << (cycleStart - data) << " -> item = "<< *pointer << " ";
+      int tmp = *pointer;
+      *pointer = element;
+      element = tmp;
+      std::cout << *pointer;
+    }
+
+    // Ciclo para el resto de elementos
+    while(pointer - data != cycleStart - data) {
+      pointer = cycleStart;
+
+      // Encuentra la posición donde ponemos el elemento
+      for(int* nextElement = cycleStart + 1; nextElement < data + size; nextElement++)
+        if(*nextElement < element)
+          pointer++;
+
+      // Ignorar elementos duplicados
+      while(element == *pointer)
+        pointer++;
+
+      // Poner el valor en la posición correcta
+      if (element != *pointer) {
+        std::cout << "\nciclo: " << (cycleStart - data) << " -> item = "<< *pointer << " ";
+        int tmp = *pointer;
+        *pointer = element;
+        element = tmp;
+        std::cout << *pointer;
+      }
+    }
+  }
+
+  std::cout << "\n\nDespués de ordenar: " << std::endl;
+  for(int* e = data; e < data + size; e++) 
+    std::cout << *e << " ";
+
+  return 0;
 }
--- cycle-short/pmain.cpp
+++ cycle-short/pmain.cpp
@@ -1,81 +1,79 @@
 #include <iostream>
-#include <string>
+#include <time.h>
 
 int main()
 {
-  int data[] = {9, 7, 8, 3, 4};
+  srand(time(0));
+
+  int size = 10;
+  int data[size];// = { 90, 45, 30, 20, 10, 40, 60, 50, 5, 2 }; //caso de prueba manual
   
-  int wr,  
-      size = sizeof(data)/sizeof(*data);
-  int* len = (data + size);
-
-  for (int* c = (data + 0); c < len; c++)
-  {
-    // int p = (c - data);
-    // const int cp = p;
-    int* t = c;
-
-    int ct = 0;
-    for (int* d = c + 1; d < len; d++) {
-      if (*c > *d) {
-        t++;
-        ct++;
-      }
-    }
-
-    std::cout << *c << "   " << ct<< "   "<< *t << std::endl;
-
-    // if (cp == p) continue;
-    //
-    // while(c == c + p) p++;
-
-    // int* tmp = c;
-    // tmp = tmp + p;
-    //   
-    // std::swap(3, c); 
-  
-/*     std::cout << std::endl << *c << "*"<< p <<"*"<< *(c + p)<< std::endl; */
-
-    // wr++;
-    //
-    //     std::cout << std::endl <<"+++++++++" << std::endl;
-    //
-    //   for (int* z = data; z < len; z++)
-    //   {
-    //     std::cout << *z << "-";
-    //   }
-    //
-    //   std::cout << std::endl <<"-----------" << std::endl;
-
-    // while(cp != p)
-    // {
-    //   p = cp;
-    //   for(int* e = c + 1; e < len; e++)
-    //     if(*c > *e) p++;
-    //
-    //   while(c == c + p) p++;
-    //
-    //   tmp = c;
-    //   tmp = tmp + p;
-    //       
-    //   std::cout << *c << "/a/" << *tmp << std::endl;
-    //
-    //   std::swap(*c, *tmp);
-    //
-    //   std::cout << *c << "/b/" << *tmp << std::endl;
-    //  
-    //   wr++;
-    // }
-    //
-    //
-    // std::cout << p << std::endl;
-    // std::cout << "-------" << std::endl;
-
+  // generamos datos aleatorios
+  std::cout << "Antes de ordenar: " << std::endl;
+  for(int* e = data; e < data + size; e++) {
+    *e = rand() & 100; // comentar linea para caso de prueba manual
+    std::cout << *e << " ";
   }
 
-  // if () {
-  //   continue;
-  // }
-  //
+  std::cout << "\n\nciclo: {ciclo} -> item = {antes del cambio} {despues del cambio}" << std::endl;
+
+  for(int* cycleStart = data; cycleStart < data + (size - 1); cycleStart++) {
+    // inicializamos el valor del primer elemento
+    int element = *cycleStart;
+    int* pointer = cycleStart;
+
+    // Cuenta todos los valores más pequeños que nuestro elemento
+    for(int* nextElement = cycleStart + 1; nextElement < data + size; nextElement++) {
+      if(*nextElement < element)
+        pointer++;
+    }
+
+    // Si el elemento ya está en la posición correcta
+    if(pointer - data == cycleStart - data) {
+      std::cout << "\nciclo: " << (cycleStart - data) << " -> item = "<< *pointer << " ya está en su posición";
+      continue;
+    }
+
+    // Ignorar elementos duplicados
+    while(element == *pointer)
+      pointer++;
+
+    // Poner el valor en la posición correcta
+    if (pointer - data != cycleStart - data) {
+      std::cout << "\nciclo: " << (cycleStart - data) << " -> item = "<< *pointer << " ";
+      int tmp = *pointer;
+      *pointer = element;
+      element = tmp;
+      std::cout << *pointer;
+    }
+
+    // Ciclo para el resto de elementos
+    while(pointer - data != cycleStart - data) {
+      pointer = cycleStart;
+
+      // Encuentra la posición donde ponemos el elemento
+      for(int* nextElement = cycleStart + 1; nextElement < data + size; nextElement++)
+        if(*nextElement < element)
+          pointer++;
+
+      // Ignorar elementos duplicados
+      while(element == *pointer)
+        pointer++;
+
+      // Poner el valor en la posición correcta
+      if (element != *pointer) {
+        std::cout << "\nciclo: " << (cycleStart - data) << " -> item = "<< *pointer << " ";
+        int tmp = *pointer;
+        *pointer = element;
+        element = tmp;
+        std::cout << *pointer;
+      }
+    }
+  }
+
+  std::cout << "\n\nDespués de ordenar: " << std::endl;
+  for(int* e = data; e < data + size; e++) 
+    std::cout << *e << " ";
+
   return 0;
 }
