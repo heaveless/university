afee8f6858909507cc61f8334589c1e7566c4351
experimentallife <user@experimentallife.dev>
2022-05-30 22:02:34 -0500

    :sparkles: cc2 task complete

---
+++ _/points/1main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <stdio.h>
+
+class Operator {
+  public:
+    Operator() {}
+
+    int sum(int a, int b) {
+      printf("%s(%i,%i): ", __func__, a, b);
+      return a + b; 
+    }
+    
+    int subtraction(int a, int b) { 
+      printf("%s(%i,%i): ", __func__, a, b);
+      return a - b; 
+    }
+    
+    int multiplication(int a, int b) { 
+      printf("%s(%i,%i): ", __func__, a, b);
+      return a * b; 
+    }
+    
+    int division(int a, int b) { 
+      printf("%s(%i,%i): ", __func__, a, b);
+      return a / b; 
+    }
+};
+
+template<class T>
+struct Func {
+  typedef int (T::*Type)(int, int);
+};
+
+struct Input {
+  int a;
+  int b;
+};
+
+template<class T>
+T** genSource(int length) {
+  T** source = new T*[length];
+
+  for (T** ptr = source; ptr < source + length; ptr++) {
+    *ptr = new T();
+  }
+
+  return source;
+}
+template<class T, typename F>
+int runMethods(T* instance, F* method, int length, Input input) {
+  int accumulator = 0;
+
+  for (auto ptr = method; ptr < method + length; ptr++) {
+    int result = (instance->**ptr)(input.a, input.b);
+    accumulator += result;
+    std::cout << result << "\t\t";
+  }
+
+  return accumulator;
+}
+
+int main() {
+  int classLength = 3;
+  int funcLength = 4;
+  Input input = { 5, 6 };
+  
+  Operator** op = genSource<Operator>(classLength);
+
+  Func<Operator>::Type methods[] = { 
+    &Operator::sum, 
+    &Operator::subtraction, 
+    &Operator::multiplication, 
+    &Operator::division 
+  };
+
+  for (Operator** ptr = op; ptr < op + classLength; ptr++) {
+    int idx = ptr - op;
+    printf("op[%i] -> ", idx);
+    int result = runMethods(*op, methods, funcLength, input);
+    printf("-> %i\n", result);
+  }
+
+  return 0;
+}
