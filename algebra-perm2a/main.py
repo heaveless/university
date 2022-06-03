6a6e79a1cb3c6ca834dc9ad9afc752c65f40db04
Maycol <66503490+MaycolZx@users.noreply.github.com>
2022-06-03 00:16:31 -0500

    Update main.py

--- algebra-perm2a/main.py
+++ algebra-perm2a/main.py
@@ -2,8 +2,15 @@ from py.GEN_PRIMOS import GEN_PRIMOS
 from py.RANDOMGEN_PRIMOS import RANDOMGEN_PRIMOS
 
 def allPrimeNumber():
-    print("\n{0}:\n".format(allPrimeNumber.__name__))
-    pass
+    listaNum = [3,4,5]
+    for i in listaNum:
+        print(f"\nNumeros primos de {i} digitos:")
+        numero = (10**(i-1))
+        while(numero <= (10**i)):
+            if MILLER_RABIN(numero,50):
+                print(numero,end="--")
+            numero += 1
+    print("\n")
 
 def randomPrimeNumber():
     print("\n{0}:\n".format(randomPrimeNumber.__name__))
