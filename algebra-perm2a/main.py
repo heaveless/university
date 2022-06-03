ed6400d8711f1824ea3769892cf0befb546bad9b
experimentallife <user@experimentallife.dev>
2022-06-02 23:58:29 -0500

    :sparkles: add function log

--- algebra-perm2a/main.py
+++ algebra-perm2a/main.py
@@ -2,9 +2,12 @@ from py.GEN_PRIMOS import GEN_PRIMOS
 from py.RANDOMGEN_PRIMOS import RANDOMGEN_PRIMOS
 
 def allPrimeNumber():
+    print("\n{0}:\n".format(allPrimeNumber.__name__))
     pass
 
 def randomPrimeNumber():
+    print("\n{0}:\n".format(randomPrimeNumber.__name__))
+
     source = [ 
         16, 16, 16, 16, 16, 16, 16, 16, 16, 16,
         32, 32, 32, 32, 32, 32, 32, 32, 32, 32,
