40335fab6f1b2e9abc1fd2dbf4430bb379474c19
experimentallife <user@experimentallife.dev>
2022-09-07 14:02:26 -0700

    :construction: in progress :v

--- _/task1/sort.cpp
+++ _/task1/sort.cpp
@@ -2,6 +2,14 @@
 
 #define VALIDATE(a, b) (a > b)
 
+enum AlgorithmType
+{
+  Nothing,
+  Polymorphism,
+  Function,
+  Macro
+};
+
 class Algorithm
 {
 private:
@@ -12,8 +20,8 @@ private:
     *b = tmp;
   }
 
-public:
-  static void sortWithNothing(int *data, int length)
+  template <class T>
+  static void sort(int *data, int length, T type, AlgorithmType algorithm = AlgorithmType::Nothing)
   {
     if (length == 1)
       return;
@@ -22,60 +30,52 @@ public:
     {
       int *next = ptr + 1;
 
-      if (*ptr > *next)
+      bool isValidate = false;
+
+      switch (algorithm)
+      {
+      // case AlgorithmType::Polymorphism:
+      //   isValidate = (*type).validate(ptr, next);
+      //   break;
+      // case AlgorithmType::Function:
+      //   isValidate = (*type)(ptr, next);
+      // break;
+      case AlgorithmType::Macro:
+        isValidate = VALIDATE(*ptr, *next);
+        break;
+      default:
+        isValidate = *ptr > *next;
+        break;
+      }
+
+
+      if (isValidate)
         swap(ptr, next);
     }
 
-    sortWithNothing(data, --length);
+    sort(data, --length, type, algorithm);
+  }
+
+public:
+  static void sortWithNothing(int *data, int length)
+  {
+    sort(data, length, 0);
   }
 
   template <class T>
   static void sortWithPolymorphism(int *data, int length, T type)
   {
-    if (length == 1)
-      return;
-
-    for (int *ptr = data; ptr < data + length; ptr++)
-    {
-      int *next = ptr + 1;
-
-      if ((*type).validate(ptr, next))
-        swap(ptr, next);
-    }
-
-    sortWithPolymorphism(data, --length, type);
+    sort(data, length, type, AlgorithmType::Polymorphism);
   }
 
   template <class T>
   static void sortWithFunction(int *data, int length, T type)
   {
-    if (length == 1)
-      return;
-
-    for (int *ptr = data; ptr < data + length; ptr++)
-    {
-      int *next = ptr + 1;
-
-      if ((*type)(ptr, next))
-        swap(ptr, next);
-    }
-
-    sortWithFunction(data, --length, type);
+    sort(data, length, type, AlgorithmType::Function);
   }
 
   static void sortWithMacro(int *data, int length)
   {
-    if (length == 1)
-      return;
-
-    for (int *ptr = data; ptr < data + length; ptr++)
-    {
-      int *next = ptr + 1;
-
-      if (VALIDATE(*ptr, *next))
-        swap(ptr, next);
-    }
-
-    sortWithMacro(data, --length);
+    sort(data, length, NULL, AlgorithmType::Macro);
   }
 };
